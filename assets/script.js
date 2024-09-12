11391c69c1220f3cd601a171376203cb983d4c2e
DebsTD22 <dominguetidebora@gmail.com>
2024-09-12 09:30:22 -0300

    atualização

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+document.getElementById('open_btn').addEventListener('click', function(){
+    document.getElementById('sidebar').classList.toggle('open_sidebar');
+})
+
+document.getElementById('close_btn').addEventListener('click', function(){
+    document.getElementById('sidebar').classList.toggle('open_sidebar')
+})
+
+document.getElementById('open_btn').addEventListener('click', function(){
+    document.getElementById('black').classList.toggle('open');
+})
+
+document.getElementById('close_btn').addEventListener('click', function(){
+    document.getElementById('black').classList.toggle('open')
+})
+
